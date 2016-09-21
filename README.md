4b00f36c00254dfa04785b6def28a8601c56e30a
Benjamin Johnson <benjamin.duncan.johnson@gmail.com>
2016-09-21 15:52:33 -0400

    compute autocorrelation times for very long rosenbrock chain

--- README.md
+++ README.md
@@ -1,17 +1,19 @@
 # squish
 
 
-A simple n-d slice sampler, using a covariance matrix (or ellipsoid) to define slice directions and step sizes
+A simple n-d slice sampler, using a covariance matrix (or ellipsoid) to set step sizes and draw slice directions.
 
 ### Example
 
 The Rosenbrock function has the density:
 ```python
-p(x_1,x_2) = -(a-x_1)**2 - b * (x_2 - x_1**2)**2
+lnp(x_1,x_2) = -(a-x_1)**2 - b * (x_2 - x_1**2)**2
 ```
 which has a maximum at `x_1 = x_2 = a`.
 
-If we sample this for 10000 iterations with `squish`,
-starting at `x = (-1, 1)` with an ellipsoidal direction and step size given by `diag([a,b])` we obtain
+If we sample this for 1e6 iterations with `squish`,
+starting at `x = (-1, 1)` we obtain
 
 ![Rosenbrock](squish/rosenbrock.png?raw=True "Rosenbrock with squish")
+
+The autocorrelation times are ~300-400 in `x_1` and `x_2`, only weakly dependent on the covariance of the ellipsoid (though the number of likelihood calls changes).
--- squish/test.py
+++ squish/test.py
@@ -26,17 +26,22 @@ def test_gaussian(niter=10000, ndim=2):
     print('----\nGaussian')
     print('{} likelihood calls for {} iterations'.format(ss.nlike, niter))
     fig.show()
+    return ss
 
 
-def test_rosenbrock(niter=10000, a=1, b=100):
+def test_rosenbrock(niter=10000, a=1, b=100, save=False):
     p0 = np.array([-1, 1])
     lnp0 = rosenbrock(p0, a=a, b=b)
 
-    transform = np.linalg.cholesky(np.diag([a, b]))
+    transform = np.linalg.cholesky(np.diag([1, 1]))
     ss = SliceSampler(transform, rosenbrock, postkwargs={'a':a, 'b':b})
 
     res = [r for r in ss.sample(p0, lnp0, niter=niter)]
-    fig = triangle.corner(ss.chain, truths=[a, a], labels=[r'$x_1$', r'$x_2$'])
+    #ss.reset()
+    #ss.sample(p0, lnp0, niter=niter, storechain=True)
+
+    print('finished sampling')
+    fig = triangle.corner(ss.chain[5000:, :], truths=[a, a], labels=[r'$x_1$', r'$x_2$'])
     free = np.array(fig.axes)[~np.array([ax.has_data() for ax in fig.axes])]
     best = ss.chain[np.argmax(ss.lnprob), :]
     text = 'Rosenbrock ($a={:4.1f}$, $b={:4.1f})$\n'.format(a, b)
@@ -45,11 +50,22 @@ def test_rosenbrock(niter=10000, a=1, b=100):
     text += 'MAP: {:4.2f}, {:4.2f}'.format(*best)
     free[0].text(0.01, 0.8, text, transform=free[0].transAxes, verticalalignment='top')
     fig.show()
-    fig.savefig('rosenbrock.pdf')
+    if save:
+        fig.savefig('rosenbrock.png')
+    return ss
+
     
 if __name__ == "__main__":
     import matplotlib.pyplot as pl
     pl.rcParams['xtick.direction'] = 'in'
     pl.rcParams['xtick.direction'] = 'in'
     #test_gaussian(ndim=4)
-    test_rosenbrock()
+    ss = test_rosenbrock()
+
+    ss = test_rosenbrock(niter=int(1e6))
+    import acor
+    lag = 10 **(np.arange(4)+1)
+    tau1 = [acor.acor(ss.chain[:,1], maxlag=l)[0] for l in lag]
+    tau0 = [acor.acor(ss.chain[:,0], maxlag=l)[0] for l in lag]
+    for l, t1, t0 in zip(lag, tau1, tau0):
+        print('maxlag:{}, tau0:{}, tau1:{}'.format(l, t0, t1))
